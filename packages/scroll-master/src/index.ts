8e8746b5bf56e7d45d2b0d0584bc1c6ca20efbd8
Robbie Cook <robbieck7@gmail.com>
2020-10-21 16:06:15 +1300

    Removed junk

--- packages/scroll-master/src/index.ts
+++ packages/scroll-master/src/index.ts
@@ -1,11 +1,3 @@
 import ScrollMaster from "./scroll";
 
-// Allows import from web
-if (window) {
-  (window as any).ScrollMaster = ScrollMaster;
-}
-
-console.log('hello');
-(window as any).hello = "hi"!
-
 export default ScrollMaster;
