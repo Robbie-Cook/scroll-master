8305e828583e7d8de0b3cb78b4e3c899e96ebbdf
Robbie Cook <robbieck7@gmail.com>
2020-10-22 22:48:41 +1300

    Added basic example

--- packages/test/src/main/MainContent.tsx
+++ packages/test/src/main/MainContent.tsx
@@ -16,6 +16,22 @@ const MainContent: React.FC<MainContentProps> = (props) => {
   return (
     <React.Fragment>
       <h1>Scroll-Master</h1>
+      <h3>
+        Scroll Master is a library to help you with your auto-scrolling needs
+      </h3>
+      <p>
+        This project was forked from{" "}
+        <a href="https://github.com/rgalus/sticky-js">sticky-js</a>, with some
+        features added, e.g., the option to apply no styles, only a class name,
+        so custom styles can be applied.
+      </p>
+      <p>
+        Options listed{" "}
+        <a href="https://github.com/Robbie-Cook/scroll-master/tree/master/packages/scroll-master#options">
+          here
+        </a>
+        .
+      </p>
     </React.Fragment>
   );
 };
