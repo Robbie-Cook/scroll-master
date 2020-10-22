fe05a0e60916f480d525d463a7fc5b809accb94b
Robbie Cook <robbieck7@gmail.com>
2020-10-22 22:32:29 +1300

    Fix syntax error

--- packages/test/src/main/MainContent.tsx
+++ packages/test/src/main/MainContent.tsx
@@ -1,20 +1,23 @@
-  /** @jsx jsx */
-  import { css, jsx } from "@emotion/core"
-  import React from "react"
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import React from "react";
 
-  /**
-   * Interface for MainContent props
-   */
-  export interface MainContentProps {
-    children?: any;
-  };
+/**
+ * Interface for MainContent props
+ */
+export interface MainContentProps {
+  children?: any;
+}
 
-  /**
-   *  A MainContent component.
-   */
-  const MainContent: React.FC<MainContentProps> = (props) => {
-    return <React.Fragment>
+/**
+ *  A MainContent component.
+ */
+const MainContent: React.FC<MainContentProps> = (props) => {
+  return (
+    <React.Fragment>
       <h1>Scroll-Master</h1>
     </React.Fragment>
+  );
+};
 
-  export default MainContent
+export default MainContent;
